609c574c619f3cf0a2aedc4e8f294025f318d036
Bastian <Pohl>
2020-02-20 07:04:58 +0100

    #sdf

--- controllers/documents.ts
+++ controllers/documents.ts
@@ -3,21 +3,29 @@ import IfController from '../interfaces/controller';
 import Documents from '../models/documents'
 
 class DocumentsController implements IfController {
-   public path = "/"
+   public path = "/documents"
    public router = Router()
+   private docs: Documents
 
    constructor() {
       this.initRoutes()
+      this.docs = new Documents()
    }
 
    public initRoutes() {
       this.router.get(this.path, this.index)
+      this.router.patch(this.path, this.asignCategory)
    }
 
    index = async (req: Request, res: Response) => {
-      let docs = await new Documents()
-      let result = await docs.getAll()
-      res.json(result)
+      let result = await this.docs.getAll()
+      res.status(200).json(result)
+   }
+
+   asignCategory = async (req: Request, res: Response) => {
+      let data = req.body
+      let result = this.docs.asignCategory(data)
+      res.status(200).json(result)
    }
 }
 
