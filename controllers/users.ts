ebba05bb5cd736144cbd9057c47428725e543cfc
Bastian <Pohl>
2020-04-10 22:56:21 +0200

    create and delete users

--- controllers/users.ts
+++ controllers/users.ts
@@ -6,18 +6,21 @@ import * as hasha from 'hasha';
 
 class UserController implements IfController {
    public router = Router();
-   public path = "/user";
+   public path = "/users";
    private users: Users;
-   
+
    constructor() {
       this.initRoutes()
       this.users = new Users()
    }
-   
+
    initRoutes() {
       this.router.post('/login', this.login)
       this.router.post('/register', this.register)
       this.router.post(`/validate`, this.validate)
+      this.router.post(this.path, this.create)
+      this.router.get(this.path, this.index)
+      this.router.delete(`${this.path}/:id`, this.delete)
    }
 
    login = async (req: Request, res: Response) : Promise<any> => {
@@ -25,7 +28,7 @@ class UserController implements IfController {
          let user = req.body.user
          let pass = req.body.pass
          let result = await this.users.login(user)
-         
+
          if (result === undefined) {
             res.status(401).json("Login unbekannt")
             return
@@ -35,11 +38,11 @@ class UserController implements IfController {
             res.status(401).json('Password falsch')
             return
          }
-         
+
          let sessions = await new Sessions()
          let sessionObj = await sessions.generateToken(result.id)
          res.status(201).json(sessionObj)
-         
+
       } catch (error) {
          res.status(400).json(error)
       }
@@ -47,12 +50,12 @@ class UserController implements IfController {
 
    register = async (req: Request, res: Response) : Promise<any> => {
       try {
-         
+
       } catch (error) {
          res.status(400).json(error)
       }
    }
-   
+
    validate = async (req: Request, res: Response) : Promise<any> => {
       try {
          let token = req.body.token
@@ -78,10 +81,54 @@ class UserController implements IfController {
       }
    }
 
-   private validatePassword = async (pass, hash, salt) : Promise<Boolean> => {
-      let hashed_pass = await hasha.async(pass+salt, {algorithm: "sha256"})
+   index = async (req: Request, res: Response): Promise<any> => {
+      try {
+         const result = await this.users.index()
+         res.status(200).json(result)
+      } catch (error) {
+         res.status(400).send()
+      }
+   }
+
+   create = async (req: Request, res: Response): Promise<any> => {
+      try {
+         const salt = this.generateSalt()
+         const newUser = {
+            user: req.body.user,
+            pass_hash: await this.hashPassword(req.body.pass, salt),
+            pass_salt: salt
+         }
+         const result = await this.users.create(newUser)
+         res.status(200).json(result)
+      } catch (error) {
+         res.status(400).send()
+      }
+   }
+
+   delete = async (req: Request, res: Response): Promise<any> => {
+      try {
+         const id = req.params.id
+         const result = await this.users.delete(id)
+         res.status(200).json(result)
+      } catch (error) {
+         res.status(400).send()
+      }
+   }
+
+
+   private validatePassword = async (pass: String, hash: String, salt: String): Promise<Boolean> => {
+      let hashed_pass = await hasha.async(`${pass}${salt}`, {algorithm: "sha256"})
       return (hashed_pass === hash) ? true : false
    }
+
+   private hashPassword = async (pass: String, salt: String): Promise<String> => {
+      return await hasha.async(`${pass}${salt}`, { algorithm: "sha256" })
+   }
+
+   private generateSalt(): String {
+      return (Math.random() * 1e32).toString(36)
+   }
+
 }
 
 export default UserController
--- models/users.ts
+++ models/users.ts
@@ -21,8 +21,8 @@ class Users {
          let sql = `
              SELECT
                id, user, pass_hash, pass_salt
-            FROM 
-               users 
+            FROM
+               users
             WHERE
                user = '${data}'
             LIMIT 1
@@ -36,7 +36,51 @@ class Users {
 
    register = async(data) => {
       try {
-         
+
+      } catch (error) {
+         throw error
+      }
+   }
+
+   index = async () => {
+      try {
+         let sql = `
+            SELECT
+               id, user
+            FROM
+               users
+         `
+         let [result, fields] = await dbm.query(sql, undefined)
+         return result
+      } catch (error) {
+         throw error
+      }
+   }
+
+   create = async (data) => {
+      try {
+         const sql = `
+            INSERT INTO
+               users
+            SET ?
+         `
+         let [result, fields] = await dbm.query(sql, data)
+         return result
+      } catch (error) {
+         throw error
+      }
+   }
+
+   delete = async (id) => {
+      try {
+         const sql = `
+            DELETE FROM
+               users
+            WHERE
+               id = ${id}
+         `
+         let [result, fields] = await dbm.query(sql, undefined)
+         return result
       } catch (error) {
          throw error
       }
